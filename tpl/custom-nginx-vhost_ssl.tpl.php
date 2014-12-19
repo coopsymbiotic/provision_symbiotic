d46b8917c1c168cbb402d33d40bbccc0d34c7e81
Mathieu Lutfy <mathieu@bidon.ca>
2014-12-19 16:01:13 -0500

    Added nginx vhost tpl override, and renamed the apache vhost tpl for clarity.

---
+++ tpl/custom-nginx-vhost_ssl.tpl.php
@@ -0,0 +1,81 @@
+
+<?php if ($this->ssl_enabled && $this->ssl_key) : ?>
+
+<?php if ($this->redirection): ?>
+<?php foreach ($this->aliases as $alias_url): ?>
+server {
+  listen       <?php print "*:{$http_ssl_port}"; ?>;
+  listen       <?php print "[::]:{$http_ssl_port}"; ?>;
+<?php
+  // if we use redirections, we need to change the redirection
+  // target to be the original site URL ($this->uri instead of
+  // $alias_url)
+  if ($this->redirection && $alias_url == $this->redirection) {
+    $this->uri = str_replace('/', '.', $this->uri);
+    print "  server_name  {$this->uri};\n";
+  }
+  else {
+    $alias_url = str_replace('/', '.', $alias_url);
+    print "  server_name  {$alias_url};\n";
+  }
+?>
+  ssl                        on;
+  ssl_certificate            <?php print $ssl_cert; ?>;
+  ssl_certificate_key        <?php print $ssl_cert_key; ?>;
+  ssl_protocols              SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers                RC4:HIGH:!aNULL:!MD5;
+  ssl_prefer_server_ciphers  on;
+  keepalive_timeout          70;
+  rewrite ^ $scheme://<?php print $this->redirection; ?>$request_uri? permanent;
+}
+<?php endforeach; ?>
+<?php endif ?>
+
+server {
+  include       fastcgi_params;
+  fastcgi_param MAIN_SITE_NAME <?php print $this->uri; ?>;
+  set $main_site_name "<?php print $this->uri; ?>";
+  fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  fastcgi_param HTTPS on;
+  fastcgi_param db_type   <?php print urlencode($db_type); ?>;
+  fastcgi_param db_name   <?php print urlencode($db_name); ?>;
+  fastcgi_param db_user   <?php print urlencode($db_user); ?>;
+  fastcgi_param db_passwd <?php print urlencode($db_passwd); ?>;
+  fastcgi_param db_host   <?php print urlencode($db_host); ?>;
+  fastcgi_param db_port   <?php print urlencode($db_port); ?>;
+  listen        <?php print "*:{$http_ssl_port}"; ?>;
+  listen        <?php print "[::]:{$http_ssl_port}"; ?>;
+  server_name   <?php
+    // this is the main vhost, so we need to put the redirection
+    // target as the hostname (if it exists) and not the original URL
+    // ($this->uri)
+    if ($this->redirection) {
+      print str_replace('/', '.', $this->redirection);
+    } else {
+      print $this->uri;
+    }
+    if (!$this->redirection && is_array($this->aliases)) {
+      foreach ($this->aliases as $alias_url) {
+        if (trim($alias_url)) {
+          print " " . str_replace('/', '.', $alias_url);
+        }
+      }
+    } ?>;
+  root          <?php print "{$this->root}"; ?>;
+  ssl                        on;
+  ssl_certificate            <?php print $ssl_cert; ?>;
+  ssl_certificate_key        <?php print $ssl_cert_key; ?>;
+  ssl_protocols              SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers                RC4:HIGH:!aNULL:!MD5;
+  ssl_prefer_server_ciphers  on;
+  keepalive_timeout          70;
+  <?php print $extra_config; ?>
+  include                    <?php print $server->include_path; ?>/nginx_vhost_common.conf;
+}
+
+<?php endif; ?>
+
+<?php
+  // Generate the standard virtual host too.
+  include(provision_class_directory('Provision_Config_Nginx_Site') . '/vhost.tpl.php');
+?>
