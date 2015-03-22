84f7b36143cb49c74f4e1ba861b21f3313172aae
Mathieu Lutfy <mathieu@bidon.ca>
2015-03-22 15:07:53 -0400

    nginx: better https configurations.

--- tpl/custom-nginx-vhost_ssl.tpl.php
+++ tpl/custom-nginx-vhost_ssl.tpl.php
@@ -19,12 +19,35 @@ server {
     print "  server_name  {$alias_url};\n";
   }
 ?>
+
   ssl                        on;
   ssl_certificate            <?php print $ssl_cert; ?>;
   ssl_certificate_key        <?php print $ssl_cert_key; ?>;
-  ssl_protocols              SSLv3 TLSv1 TLSv1.1 TLSv1.2;
-  ssl_ciphers                RC4:HIGH:!aNULL:!MD5;
+  ssl_protocols              TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers                ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:!aNULL:!eNULL:!LOW:!3DES:!DES:!MD5:!EXP:!PSK:!SRP:!DSS;
+  ssl_ecdh_curve secp521r1;
   ssl_prefer_server_ciphers  on;
+
+  # Generated using:
+  # openssl dhparam -check -5 4096 -out /etc/nginx/params.4096
+  # (can be re-generated regularly in a cron job)
+  <?php if (file_exists('/etc/nginx/params.4096')) { ?>
+  ssl_dhparam /etc/nginx/params.4096;
+  <?php } ?>
+
+  ssl_session_cache shared:SSL:10m;
+  ssl_session_timeout 10m;
+
+  <?php if ($ssl_chain_cert) { ?>
+  # Required for OSCP stapling
+  resolver 8.8.8.8 valid=500s;
+
+  # Enable ocsp stapling
+  # (mechanism by which a site can convey certificate revocation information to visitors in a privacy-preserving, scalable manner)
+  ssl_stapling on;
+  ssl_trusted_certificate <?php print $ssl_chain_cert; ?>;
+  <?php } ?>
+
   keepalive_timeout          70;
   rewrite ^ $scheme://<?php print $this->redirection; ?>$request_uri? permanent;
 }
@@ -62,12 +85,36 @@ server {
       }
     } ?>;
   root          <?php print "{$this->root}"; ?>;
+
   ssl                        on;
   ssl_certificate            <?php print $ssl_cert; ?>;
   ssl_certificate_key        <?php print $ssl_cert_key; ?>;
-  ssl_protocols              SSLv3 TLSv1 TLSv1.1 TLSv1.2;
-  ssl_ciphers                RC4:HIGH:!aNULL:!MD5;
+
+  ssl_protocols              TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers                ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:!aNULL:!eNULL:!LOW:!3DES:!DES:!MD5:!EXP:!PSK:!SRP:!DSS;
+  ssl_ecdh_curve secp521r1;
   ssl_prefer_server_ciphers  on;
+
+  # Generated using:
+  # openssl dhparam -check -5 4096 -out /etc/nginx/params.4096
+  # (can be re-generated regularly in a cron job)
+  <?php if (file_exists('/etc/nginx/params.4096')) { ?>
+  ssl_dhparam /etc/nginx/params.4096;
+  <?php } ?>
+
+  ssl_session_cache shared:SSL:10m;
+  ssl_session_timeout 10m;
+
+  <?php if ($ssl_chain_cert) { ?>
+  # Required for OSCP stapling
+  resolver 8.8.8.8 valid=500s;
+
+  # Enable ocsp stapling
+  # (mechanism by which a site can convey certificate revocation information to visitors in a privacy-preserving, scalable manner)
+  ssl_stapling on;
+  ssl_trusted_certificate <?php print $ssl_chain_cert; ?>;
+  <?php } ?>
+
   keepalive_timeout          70;
   <?php print $extra_config; ?>
   include                    <?php print $server->include_path; ?>/nginx_vhost_common.conf;
